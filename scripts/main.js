1e1bf4c05ce697868606ce4da6cdb3ba66cb28de
Youssef M. Ibrahim <youssef.ibrahim22@gmail.com>
2023-01-15 21:16:23 +0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -44,7 +44,7 @@ function checkValidations() {
   form.addEventListener('submit', e => {
   	e.preventDefault()
   	fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  		.then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+  		.then(response => alert("Thanks for Contacting us! We Will Contact You Soon."))
         .catch(error => console.error('Error!', error.message))
   })
   
